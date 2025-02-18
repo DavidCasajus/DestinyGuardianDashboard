3b79054985d4c16df7da48d8c50dfa70fa69809e
David Casajus <david.casajus@gmail.com>
2025-02-18 20:38:18 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "DestinyGuardianDashboard/",
+  base: "https://davidcasajus.github.io/DestinyGuardianDashboard/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
