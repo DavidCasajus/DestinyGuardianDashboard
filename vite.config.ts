f86d8bb47e0a453bb1d861e0165be9e14e892a59
David Casajus <david.casajus@gmail.com>
2025-02-18 21:02:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  base: "/DestinyGuardianDashboard/",  // Configura la base para GitHub Pages
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const viteConfig = () => {
+
+
+  return defineConfig({
+    base: "/DestinyGuardianDashboard/",  // Configura la base para GitHub Pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  });
+};
+
+export default viteConfig;
