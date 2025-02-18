e4dc13ffc4c03b332ea678360d5c72c10396ff17
David Casajus <david.casajus@gmail.com>
2025-02-18 20:54:50 +0100

    a ver si con el dist lo pilla

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/DestinyGuardianDashboard/",
+  base: "/DestinyGuardianDashboard/",  // Configura la base para GitHub Pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
